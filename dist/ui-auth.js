6c255b93415047ef266272463c97e386b289f385
Jorge Ramírez <jorgeramirez1990@gmail.com>
2016-12-07 20:26:00 -0300

    Se actualiza la version de distribucion
    refs #DIIBSER-28

--- dist/ui-auth.js
+++ dist/ui-auth.js
@@ -64,184 +64,216 @@ function auth(AuthenticationService, AuthorizationService) {
 }
 }());
 
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc service
- * @name ui.auth.services.Authentication
- * @description
- * # Authentication
- * Service in the ui.auth.
- */
-angular
-  .module('ui.auth.services')
-  .service('AuthenticationService', AuthenticationService);
+  /**
+   * @ngdoc service
+   * @name ui.auth.services.Authentication
+   * @description
+   * # Authentication
+   * Service in the ui.auth.
+   */
+  angular
+    .module('ui.auth.services')
+    .service('AuthenticationService', AuthenticationService);
 
-AuthenticationService.$injec = ['$resource', '$rootScope', '$http', 'Config'];
+  AuthenticationService.$injec = ['$resource', 'AuthConfig'];
 
-function AuthenticationService($resource, $rootScope, $http, Config) {
-  var Authentication = $resource(Config.serverURL + '/:action', {action: '@action'});
+  function AuthenticationService($resource, AuthConfig) {
+    var Authentication = $resource(AuthConfig.serverURL + '/oauth/token');
 
-  return {
-    login: function (username, password) {
-      var auth = new Authentication({username: username, password: password});
-      return auth.$save({action: 'login'});
-    },
+    return {
+      login: login
+    };
 
-    token: function (authParams) {
+    /**
+     * Autenticacion basada en username/password
+     */
+    function login(username, password) {
       var auth = new Authentication({
-        username: authParams.username,
-        accessToken: authParams.accessToken
+        grantType: 'password',
+        username: username,
+        password: password
       });
-      return auth.$save({action: 'token'});
-    },
+      var rsp = auth.$save();
+      rsp.then(loginSuccess);
+      return rsp;
+    }
 
-    logout: function () {
-      var authParams = this.getCurrentUser();
-      var auth = new Authentication({
-        username: authParams.username,
-        accessToken: authParams.accessToken
-      });
-      $rootScope.AuthParams = {};
-      localStorage.removeItem(Config.authParamsKey);
-      return auth.$save({action: 'logout'});
-    },
+    /**
+     * Login success callback para registrar en el localStorage los
+     * datos de autenticación.
+     *
+     * @param {object} data - datos de autenticacion
+     */
+    function loginSuccess(data) {
+      localStorage.setItem(AuthConfig.preffix, JSON.stringify(data));
+    }
 
-    getCurrentUser: function () {
-      var user = $rootScope.AuthParams;
+    /**
+     * retorna la informacion de autenticacion.
+     *
+     * @returns {object}
+     */
+    function getCurrent() {
+      var user = localStorage.getItem(AuthConfig.preffix);
 
-      if (!user || Object.keys(user).length === 0) {
-        user = JSON.parse(localStorage.getItem(Config.authParamsKey)) || undefined;
-
-        if (user) {
-          $http.defaults.headers.common.Authorization = 'Bearer ' + user.accessToken;
-        }
+      if (user) {
+        user = JSON.parse(user);
       }
       return user;
-    },
-
-    isLoggedIn: function() {
-      var user = this.getCurrentUser();
-      return user && user.accessToken !== undefined;
     }
-  };
-}
 
-}());
-
-(function() {
-'use strict';
-
-/**
- * @ngdoc service
- * @name ui.auth.services.Authorization
- * @description
- * # Authorization
- * Service in the ui.auth.
- */
-angular
-  .module('ui.auth.services')
-  .service('AuthorizationService', AuthorizationService);
-  
-AuthorizationService.$inject = ['$rootScope', '$resource', '$http', 'AuthenticationService', 'Config'];
-
-function AuthorizationService($rootScope, $resource, $http, AuthenticationService, Config) {
-  var Authorization = $resource(Config.serverURL + '/authorization/:action',
-    {action: '@action'});
-
-  return {
     /**
-     * Retorna true si el usuario actual de la aplicación posee el permiso dado como
-     * parámetro.
-     **/
-    hasPermission: function (permission, userToCheck) {
-      var user = userToCheck || AuthenticationService.getCurrentUser();
-      var permissions = [];
-      
-      if (user) {
-        permissions = user.permissions || [];
-      }
-      return permissions.indexOf(permission.trim()) >= 0;
-    },
+     * helper para determinar si el user actual se encuentra autenticado.
+     */
+    function isLoggedIn() {
+      var authenticate = this.getCurrent();
+      return !!authenticate;
+    }
+  }
+} ());
 
-    hasPermissions: function(permissions, userToCheck) {
-      var self = this;
-      var authorized = true;
-      
-      for(var i = 0; i < permissions.length; i++){
-        
-        if(!self.hasPermission(permissions[i], userToCheck)){
-          authorized = false;
-          break;
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name ui.auth.services.Authorization
+   * @description
+   * # Authorization
+   * Service in the ui.auth.
+   */
+  angular
+    .module('ui.auth.services')
+    .service('AuthorizationService', AuthorizationService);
+
+  AuthorizationService.$inject = ['$resource', 'AuthenticationService', 'AuthConfig'];
+
+  function AuthorizationService($resource, AuthenticationService, AuthConfig) {
+    var Authorization = $resource(AuthConfig.serverURL + '/authorization/:action',
+      { action: '@action' });
+
+    return {
+      /**
+       * Retorna true si el usuario actual de la aplicación posee el permiso dado como
+       * parámetro.
+       **/
+      hasPermission: function (permission, userToCheck) {
+        var user = userToCheck || AuthenticationService.getCurrent();
+        var permissions = [];
+
+        if (user) {
+          permissions = user.permissions || [];
         }
+        return permissions.indexOf(permission.trim()) >= 0;
+      },
+
+      hasPermissions: function (permissions, userToCheck) {
+        var self = this;
+        var authorized = true;
+
+        for (var i = 0; i < permissions.length; i++) {
+
+          if (!self.hasPermission(permissions[i], userToCheck)) {
+            authorized = false;
+            break;
+          }
+        }
+        return authorized;
+      },
+
+      hasAnyPermissions: function (permissions, userToCheck) {
+        var self = this;
+        return _.some(permissions, function (p) {
+          return self.hasPermission(p, userToCheck);
+        });
+      },
+
+      hasRol: function (rol, userToCheck) {
+        var user = userToCheck || AuthenticationService.getCurrentUser();
+        var rols = [];
+
+        if (user) {
+          rols = user.rols || [];
+        }
+        return rols.indexOf(rol.trim()) >= 0;
+      },
+
+      principal: function () {
+        return Authorization.get({ action: 'principal' }).$promise;
+      },
+
+      // TODO: ver como implementar esto
+      // setupCredentials: function (username, requestToken, accessToken, callback) {
+      //   var AuthParams = {
+      //     username: username,
+      //     requestToken: requestToken,
+      //     accessToken: accessToken
+      //   };
+
+      //   $rootScope.AuthParams = AuthParams;
+      //   localStorage.setItem(Config.authParamsKey, JSON.stringify(AuthParams));
+      //   $http.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
+      //   // cargamos los permisos del usuario
+      //   this.principal().then(function (response) {
+      //     AuthParams.permissions = response.permisos;
+      //     AuthParams.rols = response.roles;
+      //     AuthParams.stamp = response.stamp;
+      //     localStorage.setItem(Config.authParamsKey, JSON.stringify(AuthParams));
+      //     callback(AuthParams);
+      //   });
+      // },
+
+      authorize: function (loginRequired, requiredPermissions) {
+        var user = AuthenticationService.getCurrentUser();
+
+        if (loginRequired === true && user === undefined) {
+          return this.enums.LOGIN_REQUIRED;
+        } else if ((loginRequired && user !== undefined) &&
+          (requiredPermissions === undefined || requiredPermissions.length === 0)) {
+          return this.enums.AUTHORIZED;
+        } else if (requiredPermissions) {
+          var isAuthorized = this.hasPermissions(requiredPermissions, user);
+          return isAuthorized ? this.enums.AUTHORIZED : this.enums.NOT_AUTHORIZED;
+        }
+      },
+
+      enums: {
+        LOGIN_REQUIRED: 'loginRequired',
+        NOT_AUTHORIZED: 'notAuthorized',
+        AUTHORIZED: 'authorized'
       }
-      return authorized;
-    },
-    
-    hasAnyPermissions: function(permissions, userToCheck) {
-      var self = this;
-      return _.some(permissions, function(p) {
-        return self.hasPermission(p, userToCheck);
-      });
-    },
+    };
+  }
+} ());
 
-    hasRol: function(rol, userToCheck) {
-      var user = userToCheck || AuthenticationService.getCurrentUser();
-      var rols = [];
-      
-      if (user) {
-        rols = user.rols || [];
-      }
-      return rols.indexOf(rol.trim()) >= 0; 
-    },
+(function () {
+  'use strict';
 
-    principal: function () {
-      return Authorization.get({action: 'principal'}).$promise;
-    },
+  /**
+   * Provider para configuracion ui.auth.
+   *
+   * @ngdoc service
+   * @name ui.auth.AuthConfigProvider
+   * @description
+   * # AuthConfigProvider
+   */
+  angular.module('ui.auth')
+    .provider('AuthConfigProvider', function () {
 
-    setupCredentials: function (username, requestToken, accessToken, callback) {
-      var AuthParams = {
-        username: username,
-        requestToken: requestToken,
-        accessToken: accessToken
+      var options = {};
+
+      this.config = function (opt) {
+        angular.extend(options, opt);
       };
 
-      $rootScope.AuthParams = AuthParams;
-      localStorage.setItem(Config.authParamsKey, JSON.stringify(AuthParams));
-      $http.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
-      // cargamos los permisos del usuario
-      this.principal().then(function (response) {
-        AuthParams.permissions = response.permisos;
-        AuthParams.rols = response.roles;
-        AuthParams.stamp = response.stamp;
-        localStorage.setItem(Config.authParamsKey, JSON.stringify(AuthParams));
-        callback(AuthParams);
-      });
-    },
-
-    authorize: function (loginRequired, requiredPermissions) {
-      var user = AuthenticationService.getCurrentUser();
-
-      if (loginRequired === true && user === undefined) {
-        return this.enums.LOGIN_REQUIRED;
-      } else if ((loginRequired && user !== undefined) &&
-        (requiredPermissions === undefined || requiredPermissions.length === 0)) {
-        return this.enums.AUTHORIZED;
-      } else if (requiredPermissions) {
-        var isAuthorized = this.hasPermissions(requiredPermissions, user);
-        return isAuthorized ? this.enums.AUTHORIZED : this.enums.NOT_AUTHORIZED;
-      }
-    },
-
-    enums: {
-      LOGIN_REQUIRED: 'loginRequired',
-      NOT_AUTHORIZED: 'notAuthorized',
-      AUTHORIZED: 'authorized'
-    }
-  };
-}
-}());
+      this.$get = [function () {
+        return options;
+      }];
+    });
+} ());
 
 (function() {
 'use strict';
